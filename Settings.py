a0ecaf20c20738bd7fb4f96577243adfbf6dea9a
kaseymeier <kaseymeier588@gmail.com>
2023-10-19 08:53:30 -0400

    added text and grass

---
+++ Settings.py
@@ -0,0 +1,11 @@
+
+TILE_SIZE = 64  # tiles are square height==width
+SCREEN_WIDTH = 9*TILE_SIZE
+SCREEN_HEIGHT = 8*TILE_SIZE
+SAND_HEIGHT = 20
+
+
+# COLORS
+
+WATER_COLOR = (114, 159, 232)
+SAND_COLOR = (100, 25, 0)
--- game.py
+++ game.py
@@ -1,18 +1,12 @@
 import pygame
 import sys
+import random
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
-SAND_HEIGHT = 20
-TILE_SIZE = 64  # tiles are square height==width
+from Settings import *
 
-# COLORS
-
-WATER_COLOR = (114, 159, 232)
-SAND_COLOR = (100, 25, 0)
-
-print(f"the quit even is type {pygame.QUIT}")
 pygame.init()
+
+game_font = pygame.font.Font("assets/fonts/MASQUE__.ttf", 128)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Chomp!')
@@ -22,14 +16,27 @@ pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WID
 pygame.draw.rect(screen, (0, 200, 100), (0, 75, 74, 74))
 
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (SCREEN_WIDTH / 2 - TILE_SIZE / 2, SCREEN_HEIGHT / 2 - TILE_SIZE / 2))
+sand_top = pygame.image.load("assets/images/sand_top.png").convert()
+seagrass = pygame.image.load("assets/images/seagrass.png").convert()
+sand_top.set_colorkey((0, 0, 0))
+seagrass.set_colorkey((0, 0, 0))
+# blit sand tiles across the bottom of the screen
+for i in range(int(SCREEN_WIDTH / TILE_SIZE)):
+    screen.blit(sand, (TILE_SIZE * i, SCREEN_WIDTH - TILE_SIZE))
+    screen.blit(sand_top, (TILE_SIZE * i, SCREEN_HEIGHT - (2 * TILE_SIZE)))
+for _ in range(4):
+    x = random.randint(0, SCREEN_WIDTH)
+    y = random.randint(SCREEN_HEIGHT - 2*TILE_SIZE, SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
+    screen.blit(seagrass, (x,y))
+    #draw the title CHOMP!
+
+text = game_font.render("CHOMP!", True, (255, 69, 0))
+screen.blit(text, (SCREEN_WIDTH//2 - text.get_width()//2, SCREEN_HEIGHT//2))
 
 while True:
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             print("thanks for playing")
             pygame.quit()
             sys.exit()
-
         pygame.display.flip()
