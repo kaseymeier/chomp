efb68969902389b3220cb48d8099bd1de2cc7b41
kaseymeier <kaseymeier588@gmail.com>
2023-10-25 09:18:17 -0400

    redo

--- fish.py
+++ fish.py
@@ -2,44 +2,41 @@ import pygame
 from Settings import *
 
 
-class Fish(pygame.sprite.Sprite):
-    def __init__(self, x, y,):
+class Fish:
+    def __init__(self, x, y):
         self.right_image = pygame.image.load("assets/images/fish.png").convert()
         self.right_image.set_colorkey((0, 0, 0))
         self.left_image = pygame.transform.flip(self.right_image, True, False)
         self.image = self.right_image
+        # creating a rectangle that defines where to paint my fish
         self.rect = pygame.rect.Rect(x, y, self.image.get_width(), self.image.get_height())
         self.moving_left = False
         self.moving_right = False
         self.moving_up = False
         self.moving_down = False
 
-        print("I am a brand new fish ;)")
     def update(self):
         if self.moving_left:
-            self.rect.x -= 5
+            self.rect.x -= 2
+            self.image = self.left_image
         elif self.moving_right:
-            print(f"moving right! {self.rect.x}")
-            self.rect.x += 5
-            if self.rect.x>512:
-                print("here!")
+            self.rect.x += 2
+            self.image = self.right_image
         if self.moving_up:
-            self.rect.y -= 5
+            self.rect.y -= 2
         elif self.moving_down:
-            self.rect.y += 5
-        # make sure fish is within bounds
+            self.rect.y += 2
+        # make sure this puts the fish in a valid position
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.top < 0:
             self.rect.top = 0
         if self.rect.right > SCREEN_WIDTH:
-            print("stoppe!")
             self.rect.right = SCREEN_WIDTH
-        if self.rect.left > SCREEN_HEIGHT:
-            self.rect.left = SCREEN_HEIGHT
-        if self.rect.bottom > SCREEN_HEIGHT - 2 * TILE_SIZE:
-            self.rect.bottom = SCREEN_HEIGHT - 2 * TILE_SIZE
+        if self.rect.bottom > SCREEN_HEIGHT - 2*TILE_SIZE: # account for sand
+            self.rect.bottom = SCREEN_HEIGHT - 2*TILE_SIZE
 
 
     def draw(self, screen):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+        screen.blit(self.image, self.rect)
+
--- game.py
+++ game.py
@@ -3,67 +3,61 @@ import sys
 import random
 import fish
 import minnow
-
 from Settings import *
+
+from Settings import SCREEN_WIDTH
 
 pygame.init()
 
 game_font = pygame.font.Font("assets/fonts/MASQUE__.ttf", 128)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#pygame.display.set_caption('Chomp!')
-
-
+#pygame.display.set_caption("Chomp!")
 sand = pygame.image.load("assets/images/sand.png").convert()
 sand_top = pygame.image.load("assets/images/sand_top.png").convert()
 seagrass = pygame.image.load("assets/images/seagrass.png").convert()
-
-sand_top.set_colorkey((0, 0, 0))
 seagrass.set_colorkey((0, 0, 0))
+sand_top.set_colorkey((0, 0, 0))
 
-my_fish = fish.Fish(200, 200)
+my_fish = fish.Fish(200, 200)  # create a new fish
 my_minnows = []
 for _ in range(NUM_MINNOWS):
-    my_minnows.append(minnow.Minnow(random.randint(0, SCREEN_WIDTH - TILE_SIZE))), random.randint(0, WATER_BOTTOM - TILE_SIZE
+    my_minnows.append(minnow.Minnow(random.randint(0, SCREEN_WIDTH-TILE_SIZE),
+                                    random.randint(0, WATER_BOTTOM-TILE_SIZE)))
 
-
-my_fish = fish.Fish(200, 200) #create a new fish
 background = screen.copy()
-
 clock = pygame.time.Clock()
 
-
-
 def draw_background():
+    # draw water
     background.fill(WATER_COLOR)
-    # blit sand tiles across the bottom of the background
-    for i in range(int(SCREEN_WIDTH / TILE_SIZE)):
+    # sandy bottom
+    for i in range(SCREEN_WIDTH // TILE_SIZE):
         background.blit(sand, (TILE_SIZE * i, SCREEN_HEIGHT - TILE_SIZE))
         background.blit(sand_top, (TILE_SIZE * i, SCREEN_HEIGHT - (2 * TILE_SIZE)))
+    # randomly place 4 pieces of grass along the bottom of the background
     for _ in range(4):
         x = random.randint(0, SCREEN_WIDTH)
-        y = random.randint(SCREEN_HEIGHT - 2*TILE_SIZE, SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
-        background.blit(seagrass, (x,y))
-        #draw the title CHOMP!
+        # offset the seaweed so it looks better :)
+        y = random.randint(SCREEN_HEIGHT - 2 * TILE_SIZE, SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
+        background.blit(seagrass, (x, y))
+    # draw the CHOMP! title
+    text = game_font.render("Chomp!", True, (255, 69, 0))
 
+    background.blit(text, (SCREEN_WIDTH // 2 - text.get_width() // 2, SCREEN_HEIGHT // 2))
 
-    text = game_font.render("CHOMP!", True, (255, 69, 0))
-    background.blit(text, (SCREEN_WIDTH//2 - text.get_width()//2, SCREEN_HEIGHT//2))
 
 draw_background()
 
-
-
 while True:
+    # listen for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print("thanks for playing")
             pygame.quit()
             sys.exit()
-
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                my_fish.moving_left =True
+                my_fish.moving_left = True
             if event.key == pygame.K_RIGHT:
                 my_fish.moving_right = True
             if event.key == pygame.K_UP:
@@ -80,12 +74,16 @@ while True:
             if event.key == pygame.K_DOWN:
                 my_fish.moving_down = False
 
-    screen.blit(background, (0,0))
+    # update game objects
     my_fish.update()
+    for my_minnow in my_minnows:
+        my_minnow.update()
+
+    # draw the game screen
+    screen.blit(background, (0, 0))
     my_fish.draw(screen)
     for my_minnow in my_minnows:
-        my_minnow.draw
-    my_minnow = minnow.Minnow(100, 250)
+        my_minnow.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 
